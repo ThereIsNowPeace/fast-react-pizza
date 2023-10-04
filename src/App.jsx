800dc9db69d0d087e27e06761fe84740955e9db7
Roman <Skyfer@mail.com>
2023-10-04 18:59:22 +0300

    Created <Form> and data writing action fn for CreateOrder

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,9 @@ import Menu, {
   loader as menuLoader,
 } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
-import CreateOrder from "./features/order/CreateOrder";
+import CreateOrder, {
+  action as createOrderAction,
+} from "./features/order/CreateOrder";
 import Order, {
   loader as orderLoader,
 } from "./features/order/Order";
@@ -31,7 +33,11 @@ const router = createBrowserRouter([
         errorElement: <Error />,
       },
       { path: "/cart", element: <Cart /> },
-      { path: "/order/new", element: <CreateOrder /> },
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+        action: createOrderAction,
+      },
       {
         path: "/order/:orderId",
         element: <Order />,
